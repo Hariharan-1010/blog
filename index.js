69899c9a0f604c7cb460f10c39b4ab123d6f7a6c
Hariharan-1010 <irahkrats74@gmail.com>
2024-03-06 21:27:53 +0530

    create postList

--- index.js
+++ index.js
@@ -8,23 +8,43 @@ const users = [];
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const PORT = 3000;
 const app = express();
-
+var msg = "The passwords doen not match!";
+let currentUser;
+let currentUserName="";
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render("register.ejs", {
-        title: "Register"
+        title: "Register",
+        msg: ""
     });
 });
 app.post("/login",(req, res) => {
-    
-    console.log(req.body.name);
-    res.render("login.ejs", {
-        title: "Login"
+    const obj = req.body;
+    if(obj.pass === obj.password){
+        formToObject(obj);
+        res.render("login.ejs", {
+            title: "Login"
+        });
+    } else {
+        res.render("register.ejs", {
+            title: "Register",
+            msg: msg
+        });
+    }
+   
+});
+app.get("/home", (req, res) => {
+    if(currentUser) currentUserName = currentUser['name'];
+    res.render("post.ejs");
+});
+app.post("/home", (req, res) => {
+     let user = getUser(users, currentUserName);
+    user.contents.push(req.body.textarea);
+    res.render("postList.ejs", {
+        users: users
     });
-   const obj = req.body;
-    formToObject(obj);
 });
 
 app.listen(PORT, () => {
@@ -35,12 +55,22 @@ function formToObject(obj) {
     // Create a Person object
     const temp = new Person(obj.name, obj.email, obj.pass);
     users.push(temp);
-    console.log(users);
+    currentUser = temp;
 }
 // Constructor function for Person objects
 function Person(name, email, pass) {
     this.name = name;
     this.email = email;
     this.pass = pass;
+    this.contents = [];
   }
   
+function getUser(obj, name) {
+    let user;
+    obj.forEach(usr => {
+        if(usr.name === name){
+            user = usr;
+        }  
+    });
+    return user;
+}
