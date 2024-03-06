973fbaa367d6e5051d8b12f3a2063dd73ea3667b
Hariharan-1010 <irahkrats74@gmail.com>
2024-03-06 18:08:24 +0530

    Create user array and form object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-
+const users = [];   
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const PORT = 3000;
 const app = express();
@@ -14,15 +14,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render("register.ejs", {
-        title: "Register" 
+        title: "Register"
     });
 });
 app.post("/login",(req, res) => {
+    
+    console.log(req.body.name);
     res.render("login.ejs", {
         title: "Login"
     });
+   const obj = req.body;
+    formToObject(obj);
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
+
+function formToObject(obj) {
+    // Create a Person object
+    const temp = new Person(obj.name, obj.email, obj.pass);
+    users.push(temp);
+    console.log(users);
+}
+// Constructor function for Person objects
+function Person(name, email, pass) {
+    this.name = name;
+    this.email = email;
+    this.pass = pass;
+  }
+  
