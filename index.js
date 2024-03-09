3037fea40e7714c969f53e75adef053dd96884e5
Hariharan-1010 <irahkrats74@gmail.com>
2024-03-09 15:17:51 +0530

    Done Basic version of the complete blog :)

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+//Importing Required Modules
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const users = [];   
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//constants
 const PORT = 3000;
 const app = express();
+
+const users = [];   
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const userLoggedIn = { name: "", email: "", pass: "" }; // To Track the user logged in
+
+// Variables
 var msg = "The passwords doen not match!";
-let currentUser;
-let currentUserName="";
-app.use(express.static(path.join(__dirname,"public")));
-app.use(bodyParser.urlencoded({extended: true}));
-const userLoggedIn = {
-    name: "",
-    email: "",
-    pass: ""
-};
 let isLogged = false;
 
-//Default home Route that is register
+//Declaring static path and to access from data used bodyParser
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//GET Routes
+
+//Default Route. Register page
 app.get("/", (req, res) => {
     res.render("register.ejs", {
         title: "Register",
@@ -29,9 +32,8 @@ app.get("/", (req, res) => {
     });
 });
 
-//Get route for posts
+//Home Route, where User can post if they are logged in
 app.get("/home", (req, res) => {
-    if(currentUser) currentUserName = currentUser['name'];
     res.render("post.ejs",{
         title: "Post",
         key: isLogged ? "Logout" : "Login",
@@ -39,7 +41,7 @@ app.get("/home", (req, res) => {
     });
 });
 
-//Get route for login, for buttons
+//Login Route, where User can login
 app.get("/login", (req, res) => {
     res.render("login.ejs",{
         title: "Login",
@@ -48,7 +50,7 @@ app.get("/login", (req, res) => {
     });
 });
 
-//Get for postList
+//Route, where all posts are displayed
 app.get("/postList", (req,res)=> {
     res.render("postList.ejs", {
             users: users,
@@ -57,7 +59,7 @@ app.get("/postList", (req,res)=> {
         });
 });
 
-//Get for logout
+//Route to logout and be redirected to login Route
 app.get("/logout", (req, res) => {
     userLoggedIn.name = "";
     userLoggedIn.email = "";
@@ -69,24 +71,27 @@ app.get("/logout", (req, res) => {
         key: isLogged ? "Logout" : "Login"
     });
 });
-//Post for login , still work needed
+
+//POST Routes
+
+//Users will be registered
 app.post("/register",(req, res) => {
     let obj = req.body;
-    if(obj.pass === obj.password && !checkForExistenceOfUser(users, obj.email)){
-        formToObject(obj);
-        console.log(users);
+    if(obj.pass === obj.password && !checkForExistenceOfUser(users, obj.email)){ // To determine If the user is new
+        formToObject(obj); // Create and push User Object
+        // console.log(users); // Check console for users
         res.render("login.ejs",{
             title: "Login",
             msg: "",
             key: isLogged ? "Logout" : "Login"
         }); 
-    } else if(checkForExistenceOfUser(users, obj.email) && obj.pass === obj.password){
+    } else if(checkForExistenceOfUser(users, obj.email) && obj.pass === obj.password){ // If already a user, ask them to login
         res.render("register.ejs", {
             title: "Register",
             msg: "You are already registerd!. Login to proceed",
             key: isLogged ? "Logout" : "Login"
         });
-    } else if(obj.pass !== obj.password){
+    } else if(obj.pass !== obj.password){ // If passwords mismatch
         res.render("register.ejs", {
             title: "Register",
             msg: msg,
@@ -96,10 +101,30 @@ app.post("/register",(req, res) => {
    
 });
 
-//Posting the post
+//Users will be logged in
+app.post("/login", (req, res) => {
+    let obj = req.body;
+    if(validateUser(users, obj.email, obj.pass)) { // validate user
+        updateLogger(users.find((user) => user.email === obj.email)); // update the user as logged in
+        res.render("post.ejs",{
+            title: "Post",
+            key: isLogged ? "Logout" : "Login",
+            postMsg: "Type Here ..."
+        });
+    } else {
+        res.render("login.ejs",{
+            title: "Login",
+            msg: "Username or Password is wrong else register first!",
+            key: isLogged ? "Logout" : "Login"
+        });
+    }
+});
+
+
+//Users post will be added in the postList
 app.post("/home", (req, res) => {
-     let user = users.findIndex((user) => user.email === userLoggedIn.email);
-     if(user != -1 && req.body.textarea != ""){
+     let user = users.findIndex((user) => user.email === userLoggedIn.email); // Find user logged in
+     if(user != -1 && req.body.textarea != ""){ // If logged in and they are not submitting empty posts
         try {
             users[user].contents.push(req.body.textarea);
             res.render("post.ejs",{
@@ -107,7 +132,7 @@ app.post("/home", (req, res) => {
                 key: isLogged ? "Logout" : "Login",
                 postMsg: "Type Here ..."
             });
-        } catch (error) {
+        } catch (error) { // If they are not logged in
             console.log(error);
             res.render("login.ejs",{
                 title: "Login",
@@ -124,33 +149,15 @@ app.post("/home", (req, res) => {
      }
 });
 
-//post for login
-app.post("/login", (req, res) => {
-    let obj = req.body;
-    if(validateUser(users, obj.email, obj.pass)) {
-        updateLogger(users.find((user) => user.email === obj.email));
-        res.render("post.ejs",{
-            title: "Post",
-            key: isLogged ? "Logout" : "Login"
-        });
-    } else {
-        res.render("login.ejs",{
-            title: "Login",
-            msg: "Username or Password is wrong else register first!",
-            key: isLogged ? "Logout" : "Login"
-        });
-    }
-});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
 
 function formToObject(obj) {
-    // Create a Person object
-    const temp = new Person(obj.name, obj.email, obj.pass);
-    users.push(temp);
-    currentUser = temp;
+    const temp = new Person(obj.name, obj.email, obj.pass);     // Create a Person object
+    users.push(temp); // Push to user Array
 }
 // Constructor function for Person objects
 function Person(name, email, pass) {
@@ -160,20 +167,7 @@ function Person(name, email, pass) {
     this.contents = [""];
   }
   
-function getUser(arrayOfObj, name) {
-    if(arrayOfObj.length > 0){
-        let user;
-        for(let i in arrayOfObj) {
-            if(arrayOfObj[i].name === name){
-                user = i;
-            }  
-        };
-        // console.log(`userin getUser ${user}`);
-        return user;
-    }
-    else return "";
-}
-
+//Function to check if user in users array
 function checkForExistenceOfUser(users, email) {
     if(users.length > 0){
         for (let user in users) {
@@ -184,6 +178,8 @@ function checkForExistenceOfUser(users, email) {
     }
     return false;
 }
+
+//Function to validate user
 function validateUser(users, email, password) {
     if(users.length <=0 ){
         return false;
@@ -195,6 +191,7 @@ function validateUser(users, email, password) {
     }
 }
 
+//Function to update userLoggedIn object
 function updateLogger(user){
     userLoggedIn.name = user.name;
     userLoggedIn.email = user.email;
